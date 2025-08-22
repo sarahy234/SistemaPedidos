e185f79e4ab108bda71922cf0f6bbdc8668778b1
sarahy234 <lis2029507@est.univalle.edu>
2025-08-22 12:39:24 -0400

    Agregar archivos de proyecto.

---
+++ SistemaPedidos.Entities/PedidoList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.Entities
+{
+    public class PedidoList
+    {
+        public int num_Empl { get; set; }
+        public string? nombre { get; set; }
+        public int edad { get; set; }
+        public string? cargo { get; set; }
+        public DateOnly fechaDContrato { get; set; }
+        public int couta { get; set; }
+        public int ventas { get; set; }
+    }
+}
---
+++ SistemaPedidos.Entities/RegistroVentasCLS.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaPedidos.Entities
+{
+    public class RegistroVentasCLS
+    {
+        [Required(ErrorMessage = "El Numero del empleado es requerido")]
+        public int Num_Empl { get; set; }
+        [Required(ErrorMessage = "El Nombre del empleado es requerido")]
+        public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "El Cargo del empleado es requerido")]
+        public string  Cargo { get; set; }=null!;
+        [Required(ErrorMessage = "La fecha de cntrato del empleado es requerido")]
+        public DateOnly FechaDeContrato { get; set; }
+        [Required(ErrorMessage = "La cuota del empleado es requerido")]
+        public int Cuota { get; set; }
+        [Required(ErrorMessage = "las ventas del empleado es requerido")]
+        public int Ventas { get; set; }
+    }
+}
